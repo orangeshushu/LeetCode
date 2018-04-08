66466b13415c828860e22b6d3d7675264307e354
orangeshushu <751563925@qq.com>
2018-04-08 18:05:02 -0500

    April 8

---
+++ 167. Two Sum II - Input array is sorted.py
@@ -0,0 +1,24 @@
+'''
+Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+
+You may assume that each input would have exactly one solution and you may not use the same element twice.
+
+Input: numbers={2, 7, 11, 15}, target=9
+Output: index1=1, index2=2
+'''
+
+class Solution:
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dic = {}
+        for i in range(len(numbers)):
+            if target - numbers[i] in dic:
+                return [dic[target - numbers[i]] + 1, i + 1]
+            dic[numbers[i]] = i
+
--- Playground.py
+++ Playground.py
@@ -1,17 +1,6 @@
-import math
-points = [[0,0],[0,1],[1,0],[0,2],[2,0]]
-maxarea = 0
-for p1 in range(len(points)):
-    p2 = p1 + 1
-    p3 = len(points) - 1
-    while p2 < p3:
-        p1p2 = math.sqrt(pow((points[p1][0] - points[p2][0]), 2) + pow((points[p1][1] - points[p2][1]), 2))
-        p1p3 = math.sqrt(pow((points[p1][0] - points[p3][0]), 2) + pow((points[p1][1] - points[p3][1]), 2))
-        p2p3 = math.sqrt(pow((points[p2][0] - points[p3][0]), 2) + pow((points[p2][1] - points[p3][1]), 2))
-        if (p1p2 + p1p3 > p2p3) and (p1p2 + p2p3 > p1p3) and (p1p3 + p2p3 > p1p2):
-            s = (p1p2 + p1p3 + p2p3)/2
-            area = math.sqrt(s*(s-p1p2)*(s-p1p3)*(s-p2p3))
-            if maxarea < area:
-                maxarea = area
-        p2 = p2 + 1
-print(maxarea)
+nums = [2, 11, 8, 15]
+target = 9
+for i in nums:
+    if (target - i) in nums.remove(i):
+        print(nums)
+        print([nums.index(i), nums.index(target - i)])
