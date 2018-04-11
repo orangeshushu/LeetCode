247d78f0ffd6dd7d9597e0444cf9939d2983e0b8
orangeshushu <751563925@qq.com>
2018-04-10 19:26:08 -0500

    April 10

--- 697. Degree of an Array.py
+++ 697. Degree of an Array.py
@@ -26,6 +26,7 @@ class Solution:
         starts = {}
         ends = {}
         max_count = -sys.maxsize
+
         for i, num in enumerate(nums):
             if num not in starts:
                 starts[num] = i
@@ -34,6 +35,7 @@ class Solution:
             ends[num] = i
             max_count = max(max_count, counts[num])
         result = sys.maxsize
+
         for num, count in counts.items():
             if count == max_count:
                 result = min(result, ends[num] - starts[num] + 1)
