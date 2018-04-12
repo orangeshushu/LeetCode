47ec6d886b356de793dd2a967734577fd72f597d
orangeshushu <751563925@qq.com>
2018-04-12 17:01:03 -0500

    April 12

---
+++ 349. Intersection of Two Arrays.py
@@ -0,0 +1,24 @@
+'''
+Given two arrays, write a function to compute their intersection.
+
+Example:
+Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+
+Note:
+Each element in the result must be unique.
+The result can be in any order.
+'''
+
+
+class Solution:
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        res = []
+        for i in nums1:
+            if i in nums2 and i not in res:
+                res.append(i)
+        return res
