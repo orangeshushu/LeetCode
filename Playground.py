1b0a82148f046135fddebbe0c42beae7c2819e83
orangeshushu <751563925@qq.com>
2018-04-13 16:35:46 -0500

    April 13

--- Playground.py
+++ Playground.py
@@ -7,7 +7,7 @@ t(a[::-2])
 
 [0, 1, 2, 3, 4, 5, 6, 7, 8]
 [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-'''
+
 
 print(sys.maxsize)
 print(2**31)
@@ -16,3 +16,6 @@ class Python:
         print('Python,why self?')
 p = Python()
 p.selfDemo()
+'''
+letter = 'Abc Ick'
+print(letter.islower())
