46619d62c0c77589d535df3e5a851194f1effc34
orangeshushu <751563925@qq.com>
2018-04-23 09:22:10 -0500

    April 23

---
+++ 387. First Unique Character in a String.py
@@ -0,0 +1,25 @@
+'''
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+Note: You may assume the string contain only lowercase letters.
+'''
+import collec
+
+class Solution:
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        res = collections.Counter(s)
+        for i in range(len(s)):
+            if res[s[i]] == 1:
+                return i
+        return -1
