b400f167bea6602d68255be2e5ed0b5b9e687872
orangeshushu <751563925@qq.com>
2018-04-23 17:58:53 -0500

    April 23

--- 766. Toeplitz Matrix.py
+++ 766. Toeplitz Matrix.py
@@ -40,3 +40,10 @@ class Solution:
                 if len(res[i - j]) > 1:
                     return False
         return True
+
+# Method 2 brute force
+#         for i in range(len(matrix) - 1):
+#             for j in range(len(matrix[0]) - 1):
+#                 if matrix[i][j] != matrix[i + 1][j + 1]:
+#                     return False
+#         return True
