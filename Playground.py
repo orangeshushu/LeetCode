b2fd0fb9b3781404c14ce644dee9883c607a5af5
orangeshushu <751563925@qq.com>
2018-06-10 22:50:17 -0500

    June 10

--- Playground.py
+++ Playground.py
@@ -1,106 +1,110 @@
-import sys
-'''
-a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-print(a[:-1])
-print(a[::-1])
-t(a[::-2])
-
-[0, 1, 2, 3, 4, 5, 6, 7, 8]
-[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-
-
-print(sys.maxsize)
-print(2**31)
-class Python:
-    def selfDemo():
-        print('Python,why self?')
-p = Python()
-p.selfDemo()
-'''
-# # letter = 'Abc Ick'
-# # print(letter.islower())
-# import bisect
-# data = [4, 2, 9, 7]
-# data.sort()
-# bisect.insort
-
-# s = 'abc'
-# print(s*3)
-N = 857
-ans = 0
-# a = map(int, '11')
-# print(a)
-# for i in a:
-#     print(i)
-# <map object at 0x105b07ba8>
-# <class 'int'>
-# <class 'int'>
-
-# print([0]*3)
-# class Solution:
-#     def climbStairs(self, n):
-#         """
-#         :type n: int
-#         :rtype: int
-#         """
-#         return self.d(n)
+# import sys
+# '''
+# a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# print(a[:-1])
+# print(a[::-1])
+# t(a[::-2])
 #
-#     def d(self, a):
-#         if a ==1 or a ==2 or a == 0:
-#             return a
-#         else:
-#             return self.d(a - 1) +self.d(a - 2)
+# [0, 1, 2, 3, 4, 5, 6, 7, 8]
+# [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+#
+#
+# print(sys.maxsize)
+# print(2**31)
+# class Python:
+#     def selfDemo():
+#         print('Python,why self?')
+# p = Python()
+# p.selfDemo()
+# '''
+# # # letter = 'Abc Ick'
+# # # print(letter.islower())
+# # import bisect
+# # data = [4, 2, 9, 7]
+# # data.sort()
+# # bisect.insort
+#
+# # s = 'abc'
+# # print(s*3)
+# N = 857
+# ans = 0
+# # a = map(int, '11')
+# # print(a)
+# # for i in a:
+# #     print(i)
+# # <map object at 0x105b07ba8>
+# # <class 'int'>
+# # <class 'int'>
+#
+# # print([0]*3)
+# # class Solution:
+# #     def climbStairs(self, n):
+# #         """
+# #         :type n: int
+# #         :rtype: int
+# #         """
+# #         return self.d(n)
+# #
+# #     def d(self, a):
+# #         if a ==1 or a ==2 or a == 0:
+# #             return a
+# #         else:
+# #             return self.d(a - 1) +self.d(a - 2)
+# # if __name__ == '__main__':
+# #     s = Solution()
+# #     print(s.climbStairs(9))
+# #
+# #
+# # from collections import Counter
+# #
+# # s = '''A Counter is a dict subclass for counting hashable objects. It is an unordered collection where elements are stored as dictionary keys and their counts are stored as dictionary values. Counts are allowed to be any integer value including zero or negative counts. The Counter class is similar to bags or multisets in other languages.'''.lower()
+# #
+# # c = Counter(s)
+# # # 获取出现频率最高的5个字符
+# # print(c)
+# # print(c['a'])
+# #
+# #
+# # # Result:
+# # [(' ', 54), ('e', 32), ('s', 25), ('a', 24), ('t', 24)]
+# # 排列组合
+# # from scipy.special import comb
+# # res = 0
+# # for i in range(1,26):
+# #     res += comb(25,i)
+# # print(res)
+#
+# #
+# # num = 38
+# # a = map(int,str(num))
+# # print(sum(a))
+#
+# class Solution(object):
+#     def longestWord(self, words):
+#         """
+#         :type words: List[str]
+#         :rtype: str
+#         """
+#         while len(words) > 0:
+#             a = max(words)
+#             print(a)
+#             print(self.isright(a, words))
+#             if self.isright(a, words):
+#                 return a
+#             else:
+#                 words.remove(a)
+#
+#     def isright(self, w, s):
+#         for i in range(len(w)):
+#             if w[0:(i+1)] not in s:
+#                 return False
+#         return True
 # if __name__ == '__main__':
 #     s = Solution()
-#     print(s.climbStairs(9))
-#
-#
-# from collections import Counter
-#
-# s = '''A Counter is a dict subclass for counting hashable objects. It is an unordered collection where elements are stored as dictionary keys and their counts are stored as dictionary values. Counts are allowed to be any integer value including zero or negative counts. The Counter class is similar to bags or multisets in other languages.'''.lower()
-#
-# c = Counter(s)
-# # 获取出现频率最高的5个字符
-# print(c)
-# print(c['a'])
-#
-#
-# # Result:
-# [(' ', 54), ('e', 32), ('s', 25), ('a', 24), ('t', 24)]
-# 排列组合
-# from scipy.special import comb
-# res = 0
-# for i in range(1,26):
-#     res += comb(25,i)
-# print(res)
+#     words = ["b","br","bre","brea","break","breakf","breakfa","breakfas","breakfast","l","lu","lun","lunc","lunch","d","di","din","dinn","dinne","dinner"]
+#     answer = s.longestWord(words)
+#     print(answer)
 
-#
-# num = 38
-# a = map(int,str(num))
-# print(sum(a))
-
-class Solution(object):
-    def longestWord(self, words):
-        """
-        :type words: List[str]
-        :rtype: str
-        """
-        while len(words) > 0:
-            a = max(words)
-            print(a)
-            print(self.isright(a, words))
-            if self.isright(a, words):
-                return a
-            else:
-                words.remove(a)
-
-    def isright(self, w, s):
-        for i in range(len(w)):
-            if w[0:(i+1)] not in s:
-                return False
-        return True
-if __name__ == '__main__':
-    s = Solution()
-    words = ["b","br","bre","brea","break","breakf","breakfa","breakfas","breakfast","l","lu","lun","lunc","lunch","d","di","din","dinn","dinne","dinner"]
-    answer = s.longestWord(words)
-    print(answer)
+S = 'abc'
+S.replace(0,'d')
+print(S)
