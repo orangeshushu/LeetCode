5b271b190e675c7b5c7c786df0d41656846fe63c
orangeshushu <751563925@qq.com>
2020-05-26 00:01:46 -0500

    modify the solution

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -19,8 +19,11 @@ integer range: [−231,  231 − 1]. For the purpose of this problem, assume tha
 0 when the reversed integer overflows.
 '''
 
-a =120
-res = ''
-for i in str(a)[::-1]:
-    res = res + i
-print(int(res))
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        if (0 < x < 2147483648) and (0< int(str(x)[::-1]) < 2147483648):
+            return int(str(x)[::-1])
+        elif (-2147483648 < x < 0) and ( -2147483648 < int("-" + str(x)[::-1][:-1]) < 0):
+            return int("-" + str(x)[::-1][:-1])
+        return 0
